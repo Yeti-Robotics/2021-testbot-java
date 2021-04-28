d74908700c550b370ffc01bda6feffd79fe2bfcc
alice15311 <57412796+alice15311@users.noreply.github.com>
2021-04-27 20:46:38 -0400

    created intake subsystem pt2

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -11,15 +12,44 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
+
+  public enum IntakeStatus{ RETRACTED, EXTENDED }
+  public IntakeStatus intakeStatus;
   private DoubleSolenoid intakePistons;
   private VictorSPX intakeVictor;
+
   /** Creates a new IntakeSubsystem. */
   public IntakeSubsystem() {
     intakePistons = new DoubleSolenoid(Constants.IntakeConstants.INTAKE_PISTONS[0], Constants.IntakeConstants.INTAKE_PISTONS[1]);
+    intakeVictor = new VictorSPX(Constants.IntakeConstants.INTAKE_VICTOR);
+    intakeStatus = IntakeStatus.EXTENDED;
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  //victor methods
+  public void intakeIn(){
+    intakeVictor.set(ControlMode.PercentOutput, Constants.IntakeConstants.INTAKE_IN_POWER);
   }
+  public void moveIntake(double power){
+    intakeVictor.set(ControlMode.PercentOutput, power);
+  }
+  public void intakeOut(){
+    intakeVictor.set(ControlMode.PercentOutput, Constants.IntakeConstants.INTAKE_OUT_POWER);
+  }
+  public void intakeStop(){
+    intakeVictor.set(ControlMode.PercentOutput, 0);
+  }
+
+  //piston methods
+  public void retractIntake(){
+    intakePistons.set(DoubleSolenoid.Value.kReverse);
+    intakeStatus = IntakeStatus.RETRACTED;
+  }
+  public void extendIntake(){
+    intakePistons.set(DoubleSolenoid.Value.kForward);
+    intakeStatus = IntakeStatus.EXTENDED;
+  }
+  public IntakeStatus getItakeStatus(){
+    return intakeStatus;
+  }
+  
 }
